ca5fa49ebc22ad7be85f20f7f5f1489fba1ce9e6
jotaaave <marbenebela@gmail.com>
2026-01-05 15:49:36 -0300

    feat\ remove duplicated gym-api and added portfolio project

--- src/container/sample-projects/sample-projects.tsx
+++ src/container/sample-projects/sample-projects.tsx
@@ -1,7 +1,8 @@
 import TitleAppear from "@/components/titles/title-appear";
 import Card3DPerspective from "./3d-card";
 import academyApi from '@/../public/AcademyApiImage.png'
-import { TbBrandTypescript } from "react-icons/tb";
+import portfolioImage from '@/../public/meu_portfolio.png'
+import { TbBrandNextjs, TbBrandTailwind, TbBrandTypescript } from "react-icons/tb";
 import { SiFastify } from "react-icons/si";
 import { GiJesterHat } from "react-icons/gi";
 import { BiLogoPostgresql } from "react-icons/bi";
@@ -14,7 +15,7 @@ export default function SampleProjectView() {
                 <div className="w-50 m-20 flex flex-row">
                     <Card3DPerspective
                         title="Gym api"
-                        description=""
+                        description="Gympass-style REST API for gym management and check-ins."
                         image={academyApi}
                         progress={false}
                         langs={[
@@ -27,32 +28,17 @@ export default function SampleProjectView() {
                         msg="I developed this RESTful API using Fastify and TypeScript, focusing on a scalable structure with Clean Architecture and SOLID principles. The application uses Prisma and PostgreSQL for data persistence and JWT for secure authentication. The core of the project is validated by a rigorous suite of unit and integration tests with Vitest, ensuring that complex business rules"
                     />
                     <Card3DPerspective
-                        title="Gym api"
-                        description=""
-                        image={academyApi}
-                        progress={false}
+                        title="My Portfolio"
+                        description="My personal corner of the web. A showcase of full-stack projects"
+                        image={portfolioImage}
+                        progress={true}
                         langs={[
                             <TbBrandTypescript key={'typescript'} className="w-10 h-15" />,
-                            <SiFastify key={'fastify'} className="w-10 h-15"/>,
-                            <GiJesterHat key={'jester'} className="w-10 h-15"/>,
-                            <BiLogoPostgresql key={'postgres'} className="w-10 h-15"/>
+                            <TbBrandNextjs key={'nextjs'} className="w-10 h-15" />,
+                            <TbBrandTailwind key={'tailwind'} className="w-10 h-15" />
                         ]}
-                        url="https://github.com/jotaaave/gym-api-rest"
-                        msg="I developed this RESTful API using Fastify and TypeScript, focusing on a scalable structure with Clean Architecture and SOLID principles. The application uses Prisma and PostgreSQL for data persistence and JWT for secure authentication. The core of the project is validated by a rigorous suite of unit and integration tests with Vitest, ensuring that complex business rules"
-                    />
-                    <Card3DPerspective
-                        title="Gym api"
-                        description=""
-                        image={academyApi}
-                        progress={false}
-                        langs={[
-                            <TbBrandTypescript key={'typescript'} className="w-10 h-15" />,
-                            <SiFastify key={'fastify'} className="w-10 h-15"/>,
-                            <GiJesterHat key={'jester'} className="w-10 h-15"/>,
-                            <BiLogoPostgresql key={'postgres'} className="w-10 h-15"/>
-                        ]}
-                        url="https://github.com/jotaaave/gym-api-rest"
-                        msg="I developed this RESTful API using Fastify and TypeScript, focusing on a scalable structure with Clean Architecture and SOLID principles. The application uses Prisma and PostgreSQL for data persistence and JWT for secure authentication. The core of the project is validated by a rigorous suite of unit and integration tests with Vitest, ensuring that complex business rules"
+                        url="https://github.com/jotaaave/joaodev-portfolio"
+                        msg="I developed this personal portfolio using Next.js and TypeScript, focusing on high performance and a seamless user experience through modern web standards. The application utilizes Tailwind CSS for a refined, mobile-first UI and Framer Motion for interactive animations, ensuring a polished and responsive design. By integrating SEO best practices and optimized assets, this project not only showcases my technical expertise and full-stack journey but also serves as a benchmark for clean, maintainable frontend architecture and component-based development."
                     />
                 </div>
             </div>
